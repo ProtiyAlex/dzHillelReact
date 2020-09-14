797edb5943a75d59668ec65407f3c8dce13d24b1
ProtiyAlex <60171936+ProtiyAlex@users.noreply.github.com>
2020-09-14 12:34:35 +0300

    sub_Comp

--- dz_1/my-app/src/components/main/Main.js
+++ dz_1/my-app/src/components/main/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./css/main.css";
+import Content from "./subComponets/content/Content";
+import LeftMenu from "./subComponets/leftmenu/Leftmenu";
 
 export default class Main extends React.Component {
   render() {
     return (
       <div className="app-main">
-        <div className="app-main-left-menu">main-left-menu</div>
-        <div className="app-main-content">main-content</div>
+        <LeftMenu />
+        <Content />
       </div>
     );
   }
---
+++ dz_1/my-app/src/components/main/subComponets/leftmenu/Leftmenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./leftmenu.css";
+
+export default class LeftMenu extends React.Component {
+  render() {
+    return (
+      <div className="app-main-left-menu">
+        <p>Menu</p>
+      </div>
+    );
+  }
+}
